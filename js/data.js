94f9f66b2874ba321101deaf967fa67989166fc8
konick <konick.24@gmail.com>
2025-04-22 17:23:16 +0300

    Вёрстка страницы авторизации

---
+++ js/data.js
@@ -0,0 +1,25 @@
+/*
+массив объуктов с сотрудниками
+у одного сотрудника есть следующие данные:
+id,
+ФИО,
+post(должность),
+skill(навыки(какие самолёты он обучен обслуживать))
+*/
+
+const EMPLOYEE = [
+  {
+    id: 1,
+    lastName: '',
+    firstName: '',
+    patronymin: '',
+    post: '',
+    skill: '',
+    auth: {
+      login: '',
+      password: '',
+    },
+  }
+]
+
+export {EMPLOYEE}
